959a797b474617dce96ce96f3e36759961295984
mat2718 <matthewterry68.mt@gmail.com>
2021-09-08 18:05:01 -0400

    Update ProductDetails.tsx

--- src/screens/shop/ProductDetails.tsx
+++ src/screens/shop/ProductDetails.tsx
@@ -8,24 +8,27 @@ import {
   ScrollView,
 } from "react-native";
 import { RootStateOrAny, useSelector, useDispatch } from "react-redux";
-import Colors from "../../constants/colors"
-import * as cartActions from '../../store/actions/Cart'
+import Colors from "../../constants/colors";
+import * as cartActions from "../../store/actions/Cart";
 
 const ProductDetailScreen = (props: any) => {
-
   const productId = props.navigation.getParam("productId");
-  const selectedProduct = useSelector(
-    (state: RootStateOrAny) => state.products.availableProducts.find((prod: any) => prod.id === productId)
+  const selectedProduct = useSelector((state: RootStateOrAny) =>
+    state.products.availableProducts.find((prod: any) => prod.id === productId)
   );
   const dispatch = useDispatch();
 
   return (
     <ScrollView>
-      <Image style={styles.image} source={{uri: selectedProduct.imageUrl}}/>
+      <Image style={styles.image} source={{ uri: selectedProduct.imageUrl }} />
       <View style={styles.actions}>
-      <Button color={Colors.primary} title="Add To Cart" onPress={() => {
-        dispatch(cartActions.addToCart(selectedProduct)
-      }}/>
+        <Button
+          color={Colors.primary}
+          title="Add To Cart"
+          onPress={() => {
+            dispatch(cartActions.addToCart(selectedProduct));
+          }}
+        />
       </View>
       <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
       <Text style={styles.description}>{selectedProduct.description}</Text>
@@ -49,13 +52,13 @@ const styles = StyleSheet.create({
     color: "#888",
     textAlign: "center",
     marginVertical: 20,
-    fontFamily: "open-sans-bold"
+    fontFamily: "open-sans-bold",
   },
   description: {
     fontSize: 14,
     textAlign: "center",
     marginHorizontal: 20,
-    fontFamily: "open-sans-regular"
+    fontFamily: "open-sans-regular",
   },
   actions: {
     marginVertical: 10,
